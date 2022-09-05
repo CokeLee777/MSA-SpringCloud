9a03da6b71f91ab39c265b0e9bb50b7f2e33dbcc
CokeLee777 <akak640@kyonggi.ac.kr>
2022-09-05 15:30:15 +0900

    Section 3 - API Gateway & Load Balancer

---
+++ apigateway-service/src/main/java/com/example/apigatewayservice/config/FilterConfig.java
@@ -0,0 +1,26 @@
+package com.example.apigatewayservice.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+public class FilterConfig {
+
+    // yml 파일 말고 Java 코드로 직접 라우팅 로직 작성
+//    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
+        // 사용자로부터 /first-service 에 관한 요청이 들어오면 filter들을 거치고, 특정 uri로 라우팅
+        return builder.routes()
+                .route(r -> r.path("/first-service/**")
+                        .filters(f -> f.addRequestHeader("first-request", "first-request-header")
+                                .addResponseHeader("first-response", "first-response-header"))
+                        .uri("http://localhost:8081"))
+                .route(r -> r.path("/second-service/**")
+                        .filters(f -> f.addRequestHeader("second-request", "second-request-header")
+                                .addResponseHeader("second-response", "second-response-header"))
+                        .uri("http://localhost:8082"))
+                .build();
+    }
+}
